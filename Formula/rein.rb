151f7e7cec2ad32b0746747d96b077fa7763b58e
Delamain <delamain-ai@agentmail.to>
2026-02-22 18:42:56 -0600

    bump rein to v0.2.0

--- Formula/rein.rb
+++ Formula/rein.rb
@@ -1,26 +1,26 @@
 class Rein < Formula
   desc "Declarative language and runtime for AI agent orchestration"
   homepage "https://github.com/delamain-labs/rein"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/delamain-labs/rein/releases/download/v0.1.0/rein-v0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "da7e8c95bb79ef8aa6f265168af98f3641f6a9bcbaecc50fb92c1cff2838912d"
+      url "https://github.com/delamain-labs/rein/releases/download/v0.2.0/rein-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b29d67fbe2db550bba352eb591d486466b735255f64d6a807a70cc32dbc6f881"
     else
-      url "https://github.com/delamain-labs/rein/releases/download/v0.1.0/rein-v0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "e8026518bc6653dd7481beb939316ff42a0aaf7c4f11ac63d59ff04a454e2622"
+      url "https://github.com/delamain-labs/rein/releases/download/v0.2.0/rein-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "0ef66c18109dc1feffed0fdaf8d26e09df662134693b258f39eb5ea46bbc33ed"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/delamain-labs/rein/releases/download/v0.1.0/rein-v0.1.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "d5fba213dc04a7c623831198cde12a0c09b4df3ac3d05e3cfff1b0a5e92cd989"
+      url "https://github.com/delamain-labs/rein/releases/download/v0.2.0/rein-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "1ba2d03af283cd14c1624510affe18ac182d53a8ebdfeac6ad2e3e1b51ea9b17"
     else
-      url "https://github.com/delamain-labs/rein/releases/download/v0.1.0/rein-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "bc70df20bba813760bdd0d7344e72c8cd401330b1330826964524fd8adf9fc28"
+      url "https://github.com/delamain-labs/rein/releases/download/v0.2.0/rein-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "b21a80b9362472b28c3aca8b8806fee34707f6ac9e84445eac0f15c196f9d7b2"
     end
   end
 
